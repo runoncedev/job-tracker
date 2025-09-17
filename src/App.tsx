87a86c602a0460578e4088637600b96d986359bd
runoncedev <camilorivera86@gmail.com>
2025-09-17 19:14:23 -0300

    add delete

--- src/App.tsx
+++ src/App.tsx
@@ -65,6 +65,8 @@ const apllicationCollection = createCollection<Tables<"application">>(
       const { data, error } = await supabase
         .from("application")
         .select("*")
+        // .filter("deleted_at", "is", null)
+        .order("deleted_at", { ascending: false })
         .order("applied_date", { ascending: false });
 
       if (error) {
@@ -103,7 +105,7 @@ const apllicationCollection = createCollection<Tables<"application">>(
 
       const { error } = await supabase
         .from("application")
-        .update({ deleted_at: new Date().toUTCString() })
+        .delete()
         .eq("id", original.id);
 
       if (error) {
@@ -202,6 +204,9 @@ const Applications = ({ onEditClick }: ApplicationsProps) => {
   const { data: liveData, isLoading: isLiveLoading } = useLiveQuery((q) =>
     q
       .from({ application: apllicationCollection })
+      .orderBy(({ application }) => application.deleted_at, {
+        direction: "desc",
+      })
       .orderBy(({ application }) => application.applied_date, {
         direction: "desc",
       }),
@@ -239,7 +244,7 @@ const Applications = ({ onEditClick }: ApplicationsProps) => {
         );
 
         return (
-          <Card>
+          <Card className={application.deleted_at ? "opacity-50" : ""}>
             <div className="flex min-w-0 flex-grow flex-col gap-2 py-2.5 pl-2.5">
               <div className="flex items-center gap-2">
                 <div className="overflow-hidden font-semibold text-ellipsis whitespace-nowrap">
@@ -288,23 +293,80 @@ const Applications = ({ onEditClick }: ApplicationsProps) => {
                 </div>
               </div>
             </div>
-            <Button onClick={() => onEditClick(application)} variant="icon">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="lucide lucide-pencil-icon lucide-pencil"
-              >
-                <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
-                <path d="m15 5 4 4" />
-              </svg>
-            </Button>
+            {!application.deleted_at && (
+              <Button onClick={() => onEditClick(application)} variant="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="lucide lucide-pencil-icon lucide-pencil"
+                >
+                  <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+                  <path d="m15 5 4 4" />
+                </svg>
+              </Button>
+            )}
+            {application.deleted_at && (
+              <div>
+                <Button
+                  onClick={() => {
+                    apllicationCollection.update(application.id, (draft) => {
+                      draft.deleted_at = null;
+                    });
+                  }}
+                  variant="icon"
+                  title="Restore"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="lucide lucide-rotate-ccw-icon lucide-rotate-ccw"
+                  >
+                    <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
+                    <path d="M3 3v5h5" />
+                  </svg>
+                </Button>
+                <Button
+                  onClick={() => {
+                    apllicationCollection.delete(application.id);
+                  }}
+                  variant="icon"
+                  title="Permanently delete"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="lucide lucide-trash-2"
+                  >
+                    <path d="M3 6h18" />
+                    <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+                    <line x1="10" y1="11" x2="10" y2="17" />
+                    <line x1="14" y1="11" x2="14" y2="17" />
+                  </svg>
+                </Button>
+              </div>
+            )}
           </Card>
         );
       })}
@@ -491,7 +553,12 @@ function App() {
                           type="button"
                           className="bg-red-200 hover:bg-red-300"
                           onClick={() => {
-                            apllicationCollection.delete(editingApplication.id);
+                            apllicationCollection.update(
+                              editingApplication.id,
+                              (draft) => {
+                                draft.deleted_at = new Date().toUTCString();
+                              },
+                            );
                             setOpen(false);
                           }}
                         >
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,10 +1,18 @@
+import { cn } from "@/lib/utils";
+
 type CardProps = {
   children: React.ReactNode;
+  className?: string;
 };
 
-function Card({ children }: CardProps) {
+function Card({ children, className }: CardProps) {
   return (
-    <div className="flex w-full max-w-full items-start justify-between gap-2 rounded-md border border-gray-300 bg-gray-100 dark:border-slate-700 dark:bg-slate-950">
+    <div
+      className={cn(
+        "flex w-full max-w-full items-start justify-between gap-2 rounded-md border border-gray-300 bg-gray-100 dark:border-slate-700 dark:bg-slate-950",
+        className,
+      )}
+    >
       {children}
     </div>
   );
